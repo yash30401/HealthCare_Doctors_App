51cab71600468b6a4f5a9fdcfde9fa21250f50f1
Yashveer singh <yashveersingh30401@gmail.com>
2023-08-20 15:03:35 +0530

    dep - Dependencies added binding - viewBinding implemenetd

--- app/src/main/java/com/devyash/healthcaredoctorsapp/MainActivity.kt
+++ app/src/main/java/com/devyash/healthcaredoctorsapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.devyash.healthcaredoctorsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devyash.healthcaredoctorsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
