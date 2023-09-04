98dd5f00a167096556398c0b04be00cebdb8a949
Yashveer singh <yashveersingh30401@gmail.com>
2023-09-04 17:15:50 +0530

    validation complete

--- app/src/main/java/com/devyash/healthcaredoctorsapp/ui/fragments/AuthFragment.kt
+++ app/src/main/java/com/devyash/healthcaredoctorsapp/ui/fragments/AuthFragment.kt
@@ -16,10 +16,10 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class AuthFragment : Fragment(R.layout.fragment_auth) {
 
-    private var _binding:FragmentAuthBinding?=null
+    private var _binding: FragmentAuthBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var listOfServices:MutableList<String>
+    private lateinit var listOfServices: MutableList<String>
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,6 +28,7 @@ class AuthFragment : Fragment(R.layout.fragment_auth) {
         return super.onCreateView(inflater, container, savedInstanceState)
 
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentAuthBinding.bind(view)
@@ -38,21 +39,65 @@ class AuthFragment : Fragment(R.layout.fragment_auth) {
             addChip()
         }
 
+        binding.btnRegister.setOnClickListener {
+            validateFields()
+        }
+
     }
 
+    private fun validateFields() {
+        val etMobileNo = binding.etMobileNo.text.toString()
+        val fullName = binding.tilFullNameLayout.editText?.text.toString()
+        val specialization = binding.tilSpecializationLayout.editText?.text.toString()
+        val about = binding.tilAboutLayout.editText?.text.toString()
+        val city = binding.tilCityLayout.editText?.text.toString()
+        val address = binding.tilAddressLayout.editText?.text.toString()
+        val experience = binding.tilExperienceLayout.editText?.text.toString()
+
+        //Optional
+        val email = binding.tilEmailLayout.editText?.text.toString()
+        val website = binding.tilWebsiteLayout.editText?.text.toString()
+
+        val servicesSize = listOfServices.size
+        val workingHours = binding.tilWorkingHoursLayout.editText?.text.toString()
+        val clinicVisit = binding.tilClinicVisitLayout.editText?.text.toString()
+        val videoConsult = binding.tilVideoConsultLayout.editText?.text.toString()
+
+
+
+        if (etMobileNo.isNullOrEmpty() || fullName.isNullOrEmpty() || specialization.isNullOrEmpty() || about.isNullOrEmpty()
+            || city.isNullOrEmpty() || address.isNullOrEmpty() || experience.isNullOrEmpty() || workingHours.isNullOrEmpty() || clinicVisit.isNullOrEmpty() || videoConsult.isNullOrEmpty()
+        ) {
+            Toast.makeText(requireContext(), "Empty Fields!", Toast.LENGTH_SHORT).show()
+        } else if (servicesSize == 0) {
+
+            Toast.makeText(requireContext(), "Please Add Services", Toast.LENGTH_SHORT).show()
+
+        } else {
+            registerDoctor()
+        }
+    }
+
+    private fun registerDoctor() {
+        Toast.makeText(requireContext(), "Successfull", Toast.LENGTH_SHORT).show()
+    }
+
+
+    // Adding Chips programmatically to add services
     private fun addChip() {
         val service = binding.tilServicesLayout.editText?.text.toString()
 
-        if(service == ""){
+        if (service == "") {
             Toast.makeText(requireContext(), "Please Enter Service", Toast.LENGTH_SHORT).show()
-        }else{
-            val chip  = Chip(requireContext())
+        } else {
+            val chip = Chip(requireContext())
             chip.setText(service)
             chip.chipStrokeWidth = 0F
             chip.setChipBackgroundColorResource(R.color.specialistCardBackgroundColor)
             chip.chipCornerRadius = 20F
             chip.isCloseIconVisible = true
-            chip.closeIcon = ContextCompat.getDrawable(requireContext(),R.drawable.baseline_delete_24)
+            chip.closeIcon =
+                ContextCompat.getDrawable(requireContext(), R.drawable.baseline_delete_24)
 
             binding.chipGroupServices.addView(chip)
             listOfServices.add(service)
