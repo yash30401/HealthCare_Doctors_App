333d5cf264553a0f98192dc0ee396e8ff424b504
Yashveer singh <yashveersingh30401@gmail.com>
2023-09-15 20:26:24 +0530

    util function created

--- app/src/main/java/com/devyash/healthcaredoctorsapp/repositories/AuthRepository.kt
+++ app/src/main/java/com/devyash/healthcaredoctorsapp/repositories/AuthRepository.kt
@@ -1,5 +1,6 @@
 package com.devyash.healthcaredoctorsapp.repositories
 
+import com.devyash.healthcaredoctorsapp.networking.NetworkResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.PhoneAuthCredential
@@ -9,6 +10,13 @@ class AuthRepository @Inject constructor(private val firebaseAuth: FirebaseAuth)
 
     val currentUser:FirebaseUser?= firebaseAuth.currentUser
 
-    suspend fun signinWithPhoneNumber(credential: PhoneAuthCredential):
+    suspend fun signinWithPhoneNumber(credential: PhoneAuthCredential):NetworkResult<FirebaseUser>{
+        return try{
+            val result = firebaseAuth.signInWithCredential(credential)
+            NetworkResult.Success()
+        }catch (e:Exception){
+            NetworkResult.Error(e.message)
+        }
+    }
 
 }
