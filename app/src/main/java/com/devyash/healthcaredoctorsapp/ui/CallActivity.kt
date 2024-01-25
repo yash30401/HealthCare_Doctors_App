cb43e64b41cfe55d11ee7ca82897ff888b4b0b73
Yashveer singh <yashveersingh30401@gmail.com>
2024-01-25 13:32:54 +0530

    Video Calling implementation started

---
+++ app/src/main/java/com/devyash/healthcaredoctorsapp/ui/CallActivity.kt
@@ -0,0 +1,12 @@
+package com.devyash.healthcaredoctorsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devyash.healthcaredoctorsapp.R
+
+class CallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_call)
+    }
+}
